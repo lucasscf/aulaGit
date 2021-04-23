6987facedfce6c579b642a201f11ba334fefdbb0
lucasscf <lucasscf@gmail.com>
2021-04-23 19:47:32 -0300

    envio arquivos do projeto

---
+++ SpringBDController/SpringBDController/src/test/java/com/cotemig/SpringBDController/SpringBdControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotemig.SpringBDController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBdControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
